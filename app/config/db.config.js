2e4988d2ee47349095926ad713c40271fc79019b
Teraitea <proteraitea@gmail.com>
2023-08-15 10:51:24 +0200

    initial commit

---
+++ app/config/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  HOST: process.env.MYSQL_HOST,
+  USER: process.env.MYSQL_DATABASE_NAME,
+  PASSWORD: process.env.MYSQL_DATABASE_PWD,
+  DB: process.env.MYSQL_DATABASE_NAME
+};
---
+++ app/controllers/tutorial.controller.js
@@ -0,0 +1,73 @@
+const Tutorial = require("../models/tutorial.model.js");
+
+// Create and Save a new Tutorial
+exports.create = (req, res) => {
+  // Validate request
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!"
+    });
+  }
+
+  // Create a Tutorial
+  const tutorial = new Tutorial({
+    title: req.body.title,
+    description: req.body.description,
+    published: req.body.published || false
+  });
+
+  // Save Tutorial in the database
+  Tutorial.create(tutorial, (err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Tutorial."
+      });
+    else res.send(data);
+  });
+};
+
+// Retrieve all Tutorials from the database (with condition).
+exports.findAll = (req, res) => {
+  const title = req.query.title;
+
+  Tutorial.getAll(title, (err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials."
+      });
+    else res.send(data);
+  });
+};
+
+exports.findAllPublished = (req, res) => {
+  Tutorial.getAllPublished((err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials."
+      });
+    else res.send(data);
+  });
+};
+
+// Find a single Tutorial with a id
+exports.findOne = (req, res) => {
+  
+};
+
+// Update a Tutorial identified by the id in the request
+exports.update = (req, res) => {
+  
+};
+
+// Delete a Tutorial with the specified id in the request
+exports.delete = (req, res) => {
+  
+};
+
+// Delete all Tutorials from the database.
+exports.deleteAll = (req, res) => {
+  
+};
